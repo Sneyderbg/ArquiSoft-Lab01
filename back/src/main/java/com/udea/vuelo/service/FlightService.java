ba66863e19e452693155f9cfbd4b74cb7bd2664c
sneyderbg <sneyder.buitrago@udea.edu.co>
2024-03-07 17:29:14 -0500

    Codigo de FlightService simplificado

--- back/src/main/java/com/udea/vuelo/service/FlightService.java
+++ back/src/main/java/com/udea/vuelo/service/FlightService.java
@@ -15,107 +15,73 @@ import com.udea.vuelo.model.Flight;
 public class FlightService {
 
     private final String filePath = "flights.json";
+    private Flight[] flights;
+
+    public FlightService() {
+        try {
+
+            InputStream fileStream = getClass().getClassLoader().getResourceAsStream(filePath);
+            ObjectMapper objectMapper = new ObjectMapper();
+            flights = objectMapper.readValue(fileStream, Flight[].class);
+            if (fileStream == null) {
+                throw new Exception("database file cannot be loaded.");
+            }
+        } catch (Exception e) {
+
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
     public List<List<Flight>> searchFlights(LocalDate startDate, LocalDate endDate) {
 
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            InputStream fileStream = getClass().getClassLoader().getResourceAsStream(filePath);
-            if (fileStream != null) {
-                Flight[] flights = objectMapper.readValue(fileStream, Flight[].class);
-                return Arrays.asList(Arrays.stream(flights)
-                        .filter(flight -> isDateInRange(flight.getDepartureDate(), startDate, endDate))
-                        .collect(Collectors.toList()));
-            } else {
-                throw new Exception("database file cannot be loaded.");
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        return Arrays.asList(Arrays.stream(flights)
+                .filter(flight -> isDateInRange(flight.getDepartureDate(), startDate, endDate))
+                .collect(Collectors.toList()));
 
     }
-    //Funcion que se encarga de filtrar los vuelos por precio
+
+    // Funcion que se encarga de filtrar los vuelos por precio
     public List<List<Flight>> searchFlightsByPrice(int inicialPrice, int finalPrice) {
 
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            InputStream fileStream = getClass().getClassLoader().getResourceAsStream(filePath);
-            if (fileStream != null) {
-                Flight[] flights = objectMapper.readValue(fileStream, Flight[].class);
-                return Arrays.asList(Arrays.stream(flights)
-                        .filter(flight -> isPriceInRange(flight.getPrice(), inicialPrice, finalPrice))
-                        .collect(Collectors.toList()));
-            } else {
-                throw new Exception("database file cannot be loaded.");
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        return Arrays.asList(Arrays.stream(flights)
+                .filter(flight -> isPriceInRange(flight.getPrice(), inicialPrice, finalPrice))
+                .collect(Collectors.toList()));
 
     }
-    //Funcion que se encarga de Buscar los vuelos por nombre de la aerolinea
+
+    // Funcion que se encarga de Buscar los vuelos por nombre de la aerolinea
     public List<List<Flight>> searchFlightsByName(String nameAirline) {
 
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            InputStream fileStream = getClass().getClassLoader().getResourceAsStream(filePath);
-            if (fileStream != null) {
-                Flight[] flights = objectMapper.readValue(fileStream, Flight[].class);
-                return Arrays.asList(Arrays.stream(flights)
-                        .filter(flight -> flight.getAirline().equalsIgnoreCase(nameAirline))
-                        .collect(Collectors.toList()));
-            } else {
-                throw new Exception("database file cannot be loaded.");
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        return Arrays.asList(Arrays.stream(flights)
+                .filter(flight -> flight.getAirline().equalsIgnoreCase(nameAirline))
+                .collect(Collectors.toList()));
 
     }
+
     public List<List<Flight>> searchFlightsByDestination(String placeOfDestination) {
 
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            InputStream fileStream = getClass().getClassLoader().getResourceAsStream(filePath);
-            if (fileStream != null) {
-                Flight[] flights = objectMapper.readValue(fileStream, Flight[].class);
-                return Arrays.asList(Arrays.stream(flights)
-                        .filter(flight -> flight.getDestination().equalsIgnoreCase(placeOfDestination))
-                        .collect(Collectors.toList()));
-            } else {
-                throw new Exception("database file cannot be loaded.");
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        return Arrays.asList(Arrays.stream(flights)
+                .filter(flight -> flight.getDestination().equalsIgnoreCase(placeOfDestination))
+                .collect(Collectors.toList()));
 
     }
+
     public List<List<Flight>> searchFlightsByOrigin(String placeofOrigin) {
 
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            InputStream fileStream = getClass().getClassLoader().getResourceAsStream(filePath);
-            if (fileStream != null) {
-                Flight[] flights = objectMapper.readValue(fileStream, Flight[].class);
-                return Arrays.asList(Arrays.stream(flights)
-                        .filter(flight -> flight.getOrigin().equalsIgnoreCase(placeofOrigin))
-                        .collect(Collectors.toList()));
-            } else {
-                throw new Exception("database file cannot be loaded.");
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        return Arrays.asList(Arrays.stream(flights)
+                .filter(flight -> flight.getOrigin().equalsIgnoreCase(placeofOrigin))
+                .collect(Collectors.toList()));
 
     }
 
-    // -------------------------------------- utils --------------------------------------
-    // -----------------------------------------------------------------------------------
+    // -------------------------------- utils --------------------------------
+    // -----------------------------------------------------------------------
     public boolean isDateInRange(LocalDate date, LocalDate start, LocalDate end) {
         // return (date.compareTo(start) >= 0 && date.compareTo(end) <= 0);
         return (date.isAfter(start) && date.isBefore(end));
     }
-    public boolean isPriceInRange(int price,int priceInicial,int priceFinal){
+
+    public boolean isPriceInRange(int price, int priceInicial, int priceFinal) {
         return (price >= priceInicial && price <= priceFinal);
     }
 
