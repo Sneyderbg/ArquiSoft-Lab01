568db44d4e5b8d11daf100c66d4890f02dbcf783
Samuelab2007 <90534570+Samuelab2007@users.noreply.github.com>
2024-03-08 00:34:33 -0500

    Refactor to manage requests from App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import { FlightSearch } from "./FlightSearch";
+import { Table } from "./components/Table";
 
 function App() {
+  const [flights, setFlights] = useState([]);
+  const handleSearchByDatesClick = () => {
+    const startDate = document.getElementById("startDate").value;
+    const endDate = document.getElementById("endDate").value;
+
+    const flightByDatesHeaders = new Headers({ method: "GET" });
+    const flightByDatesRequest = new Request(
+      `http://localhost:8080/flights/search?startDate=${startDate}&endDate=${endDate}`,
+      flightByDatesHeaders
+    );
+
+    fetch(flightByDatesRequest)
+      .then((response) => response.json())
+      .then((data) => setFlights(data))
+      .catch((error) => alert("Error en el servidor", error.status));
+  };
+
   return (
     <div>
-      <FlightSearch />
+      <FlightSearch handleSearch={handleSearchByDatesClick} />
+      <Table flights={flights} />
     </div>
   );
 }
--- front/src/FlightSearch.js
+++ front/src/FlightSearch.js
@@ -1,49 +1,17 @@
-import axios from "axios";
 import React, { useState } from "react";
 
-function FlightSearch() {
+function FlightSearch({ handleSearch }) {
   const [startDate, setStartDate] = useState("2024-01-01");
   const [endDate, setEndDate] = useState("2024-01-01");
 
-  const [response, setResponse] = useState({
-    flights: [],
-    loading: false,
-    success: false,
-    error: "",
-  });
-
-  const handleSearch = () => {
-    setResponse({ flights: [], loading: true, success: false, error: "" });
-    axios
-      .get(
-        `http://localhost:8080/flights/search?startDate=${startDate}&endDate=${endDate}`
-      )
-      .then((res) => {
-        setResponse({
-          flights: res.data[0],
-          loading: false,
-          success: true,
-          error: "",
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        setResponse({
-          flights: [],
-          loading: false,
-          success: false,
-          error: "Error con el servidor",
-        });
-      });
-  };
-
   return (
     <div>
-      <h1>Buscar vuelos</h1>
+      <h3>Buscar vuelos</h3>
       <div>
         <label>Fecha de inicio:</label>
         <input
           type="date"
+          id="startDate"
           value={startDate}
           onChange={({ target: { value } }) => {
             setStartDate(value);
@@ -52,38 +20,13 @@ function FlightSearch() {
         <label>Fecha de fin:</label>
         <input
           type="date"
+          id="endDate"
           value={endDate}
           onChange={({ target: { value } }) => {
             setEndDate(value);
           }}
         ></input>
         <button onClick={handleSearch}>Buscar</button>
-      </div>
-      <div>
-        {response.loading && <p>Cargando...</p>}
-        {response.error.length > 0 && <h3>response.error</h3>}
-        {response.flights.length === 0 && response.success && (
-          <p>No hay vuelos programados para estas fechas</p>
-        )}
-        {response.flights.length > 0 && (
-          <ul>
-            {response.flights.map((flight, idx) => {
-              return (
-                <>
-                  <li id={`flight#${idx}`} key={flight.id}>
-                    {`[${flight.id}] de ${flight.origin} a ${flight.destination}`}
-                    <ul>
-                      <li>{`Aerolinea: ${flight.airline}`}</li>
-                      <li>{`Sale el ${flight.departureDate}`}</li>
-                      <li>{`Llega el ${flight.arrivalDate}`}</li>
-                      <li>{`Cuesta $${flight.price}`}</li>
-                    </ul>
-                  </li>
-                </>
-              );
-            })}
-          </ul>
-        )}
       </div>
     </div>
   );
